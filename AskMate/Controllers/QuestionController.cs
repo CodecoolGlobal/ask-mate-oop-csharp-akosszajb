4c53634e37ac03f73b419547ae5e4656eac12744
bmkriszti <bmkriszti97@gmail.com>
2024-06-18 10:44:10 +0200

    should work swagger

--- AskMate/Controllers/QuestionController.cs
+++ AskMate/Controllers/QuestionController.cs
@@ -1,14 +1,27 @@
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using System.IO;
 
-namespace AskMate.Controllers;
-
-[ApiController]
-[Route("[controller]")]
-public class QuestionController : ControllerBase
+namespace AskMate.Controllers
 {
-    [HttpGet]
-    public IActionResult GetAll()
+    [ApiController]
+    [Route("[controller]")]
+    public class QuestionController : ControllerBase
     {
-        return Ok("No questions were asked yet.");
+        private readonly string _connectionString;
+
+        public QuestionController(IWebHostEnvironment env)
+        {
+            string filePath = Path.Combine(env.ContentRootPath, "ConnectionString.txt");
+
+            // Read the connection string from the file
+            _connectionString = System.IO.File.ReadAllText(filePath).Trim();
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok("No questions were asked yet.");
+        }
     }
 }
